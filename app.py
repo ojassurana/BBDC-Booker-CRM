7c512ae2bd99ecac0c38937ee3a57284f916b60b
ojassurana <me@ojassurana.com>
2023-05-22 11:21:15 +0800

    Update Payment Link ID

--- app.py
+++ app.py
@@ -423,7 +423,7 @@ async def echo(request: Request):
                             credits = client_status['credits']
                             credits_used = client_status['credits_used']
                             random_id = client_status['random_id']
-                            product_payment_code = "fZe9Dv49I1ICdFu5kk"
+                            product_payment_code = "28o16Z5dMevo1WMcMN"
                             await send_text(chat_id, msg3.format(credits=credits, credits_used=credits_used, random_id=random_id, product_payment_code=product_payment_code))
                     elif "/choose_session" == update.message.text:
                         # checks if client has credits
